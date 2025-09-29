10fa7abe50370aa9bd09651380857ee56722516d
Administrator <sanzond@hotmail.com>
2025-09-29 16:33:41 +0800

    registry.add说明

--- static/src/js/hello_demo_action.js
+++ static/src/js/hello_demo_action.js
@@ -3,5 +3,5 @@
 import { registry } from '@web/core/registry';
 import { HelloDemo } from './hello_demo/hello_demo';
 
-// 注册客户端动作 说
+// 注册客户端动作 说明：第一个参数是动作的唯一标识符，第二个参数是动作组件
 registry.category('actions').add('hello_demo_action', HelloDemo);
